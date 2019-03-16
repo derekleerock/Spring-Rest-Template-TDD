b356205f9f29e41d7982699f90219961fca68148
Derek Lee <dereklee@downtothis.com>
2019-03-16 14:13:10 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/resttemplatedemo/ResttemplatedemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.resttemplatedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResttemplatedemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResttemplatedemoApplication>(*args)
+}
