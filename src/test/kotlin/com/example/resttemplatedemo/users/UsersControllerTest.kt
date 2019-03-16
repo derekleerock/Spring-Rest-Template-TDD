83345bdcc1919fd37ac40dd2292bc3d49e9c8fa8
Derek Lee <dereklee@downtothis.com>
2019-03-16 14:23:28 +0900

    Refactors common test code

--- src/test/kotlin/com/example/resttemplatedemo/users/UsersControllerTest.kt
+++ src/test/kotlin/com/example/resttemplatedemo/users/UsersControllerTest.kt
@@ -1,29 +1,32 @@
 package com.example.resttemplatedemo.users
 
 import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Before
 import org.junit.Test
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 class UsersControllerTest {
-    @Test
-    fun getUsers_returnsOk() {
-        val mockMvc = MockMvcBuilders
+    private lateinit var mockMvc: MockMvc
+
+    @Before
+    fun setUp() {
+        mockMvc = MockMvcBuilders
                 .standaloneSetup(UsersController())
                 .build()
+    }
 
+    @Test
+    fun getUsers_returnsOk() {
         mockMvc.perform(get("/users"))
                 .andExpect(status().isOk)
     }
 
     @Test
     fun getUsers_returnsUsers() {
-        val mockMvc = MockMvcBuilders
-                .standaloneSetup(UsersController())
-                .build()
-
         mockMvc.perform(get("/users"))
                 .andExpect(jsonPath("$[0].id", equalTo(12)))
                 .andExpect(jsonPath("$[0].username", equalTo("cdavis")))
